72999e52208c0a949803a29330951cf076c9c0bc
Arne Jans <aj@solute.de>
2015-11-18 11:24:46 +0100

    fixes back-button issues #72, #75, #82 and (confirmation pending from OP) #84

--- library/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
+++ library/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
@@ -4,6 +4,7 @@ import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -85,10 +86,11 @@ public class SearchBox extends RelativeLayout {
 	private android.support.v4.app.Fragment mContainerSupportFragment;
 	private SearchFilter mSearchFilter;
 	private ArrayAdapter<? extends SearchResult> mAdapter;
+	private boolean revealedFromMenuItem;
+	private Activity activity;
 
 
-
-    /**
+	/**
 	 * Create a new searchbox
 	 * @param context Context
 	 */
@@ -114,6 +116,7 @@ public class SearchBox extends RelativeLayout {
 	public SearchBox(final Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		inflate(context, R.layout.searchbox, this);
+		activity = scanForActivity(getContext());
 		this.searchOpen = false;
 		this.isMic = true;
 		this.materialMenu = (MaterialMenuView) findViewById(R.id.material_menu_button);
@@ -274,6 +277,7 @@ public class SearchBox extends RelativeLayout {
                         activity, this);
 			}
 		}
+		revealedFromMenuItem = true;
 	}
 	
 	/***
@@ -290,7 +294,7 @@ public class SearchBox extends RelativeLayout {
 				int[] location = new int[2];
 				menuButton.getLocationInWindow(location);
 				hideCircularly(location[0] + menuButton.getWidth() * 2 / 3, location[1],
-                        activity);
+						activity);
 			}
 		}
 	}
@@ -323,6 +327,7 @@ public class SearchBox extends RelativeLayout {
             @Override
             public void onAnimationEnd() {
                 setVisibility(View.GONE);
+				revealedFromMenuItem = false;
             }
 
             @Override
@@ -393,6 +398,17 @@ public class SearchBox extends RelativeLayout {
 				mContainerSupportFragment.startActivityForResult(intent, VOICE_RECOGNITION_CODE);
 			}
 		}
+	}
+
+	private Activity scanForActivity(Context cont) {
+		if (cont == null)
+			return null;
+		else if (cont instanceof Activity)
+			return (Activity)cont;
+		else if (cont instanceof ContextWrapper)
+			return scanForActivity(((ContextWrapper)cont).getBaseContext());
+
+		return null;
 	}
 
 	/***
@@ -944,8 +960,18 @@ public class SearchBox extends RelativeLayout {
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.KEYCODE_BACK && getVisibility() == View.VISIBLE){
-            hideCircularly((Activity) getContext());
+        if(e.getKeyCode() == KeyEvent.KEYCODE_BACK &&
+				e.getAction() == KeyEvent.ACTION_UP &&
+				getVisibility() == View.VISIBLE &&
+				searchOpen) {
+			if (revealedFromMenuItem) {
+				if (activity != null) {
+					closeSearch();
+					hideCircularly(activity);
+				}
+			} else {
+				toggleSearch();
+			}
             return true;
         }
 
